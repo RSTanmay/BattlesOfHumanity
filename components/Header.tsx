01aed994a26c4beea9fd89153cd413fcd79db34d
Tanmay singh <TanmaySingh2028@gmail.com>
2026-07-20 13:38:00 +0530

    changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,14 +10,14 @@ export default function Header({ onOpenMenu }: HeaderProps) {
       {/* Left */}
       <button
         onClick={onOpenMenu}
-        className="text-3xl font-bold hover:text-gray-600 transition"
+        className="text-3xl cursor-pointer font-bold hover:text-gray-600 transition"
       >
         ☰
       </button>
 
       {/* Center */}
       <h1 className="text-3xl font-bold tracking-wide">
-        BATTLES OF HUMANITY
+        BIGGEST BATTLES OF HUMANITY🩸
       </h1>
 
       {/* Right (reserved for future features) */}
